56462df388ef013a0c0e1504fcd59363852f0aeb
geludwig <74719181+geludwig@users.noreply.github.com>
2022-02-01 13:19:36 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -14,7 +14,7 @@ if (Test-Path $fileToCheck -PathType leaf){
 }
 
 "INSTALL PYTHON PACKAGES"
-& "C:\Program Files\Python310\Scripts\pip.exe" install pandas --no-warn-script-location
+& "C:\Program Files\Python310\Scripts\pip.exe" install pandas --no-warn-script-location > NUL
 
 "DONE"
 pause
