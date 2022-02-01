ce9b70f976a46fd644631645203588898c67c007
geludwig <74719181+geludwig@users.noreply.github.com>
2022-02-01 10:59:00 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 ﻿"DOWNLOADING SCRIPT"
-wget https://raw.githubusercontent.com/geludwig/SorttData/main/SortAlgorithm.py -OutFile "$env:USERPROFILE\SortData\SortAlgorithm.py"
+wget https://raw.githubusercontent.com/geludwig/SortData/main/SortAlgorithm.py -OutFile "$env:USERPROFILE\SortData\SortAlgorithm.py"
 
 "CHECK PYTHON INSTALLATION"
 $fileToCheck = "C:\Program Files\Python310\python.exe"
@@ -7,15 +7,15 @@ if (Test-Path $fileToCheck -PathType leaf){
 	"PYTHON ALREADY INSTALLED"
 }else{
 	"INSTALLING PYTHON"
-	New-Item -ItemType directory -Path "$env:USERPROFILE\SortPatientData\temp"
-	wget https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -OutFile "$env:USERPROFILE\SortPatientData\temp\python-installer.exe"
-	Start-Process "$env:USERPROFILE\SortPatientData\temp\python-installer.exe" -argumentlist "/quiet InstallAllUsers=1 PrependPath=1" -wait
-	Remove-Item -Recurse -Force "$env:USERPROFILE\SortPatientData\temp" 
+	New-Item -ItemType directory -Path "$env:USERPROFILE\SortData\temp"
+	wget https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -OutFile "$env:USERPROFILE\SortData\temp\python-installer.exe"
+	Start-Process "$env:USERPROFILE\SortData\temp\python-installer.exe" -argumentlist "/quiet InstallAllUsers=1 PrependPath=1" -wait
+	Remove-Item -Recurse -Force "$env:USERPROFILE\SortData\temp" 
 }
 
 "INSTALL PYTHON PACKAGES"
 & "C:\Program Files\Python310\Scripts\pip.exe" install pandas
 
 "START SORT ALGORITHM"
-ii "$env:USERPROFILE\SortPatientData\"
-ii "$env:USERPROFILE\SortPatientData\SortAlgorithm.py"
+ii "$env:USERPROFILE\SortData\"
+ii "$env:USERPROFILE\SortData\SortAlgorithm.py"
